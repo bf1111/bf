2d839e3c956b291c7a99060e9e1fa558352d31ef
xiaoming <xiaoming@qq.com>
2020-07-11 22:03:36 +0800

    session8

--- common/regsession.php
+++ common/regsession.php
@@ -2,8 +2,7 @@
 require_once 'config.php';
 //判断用户是否登录
 session_start();
-if(empty($_SESSION['user_name']) || empty($_SESSION['user_id'] || empty($_SESSION)))
-{
-    echo "<script>window.location.href='".LOGINPAGE."'</script>";
+if ($_SESSION['user_name'] == "" || $_SESSION['user_id'] == "") {
+    echo "<script>window.location.href='" . LOGINPAGE . "'</script>";
     exit;
 }
