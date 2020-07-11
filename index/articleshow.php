277d73583a3106f2761acc71a1ad0097c6b702a8
xiaoming <xiaoming@qq.com>
2020-07-11 22:47:17 +0800

    44433

--- index/articleshow.php
+++ index/articleshow.php
@@ -146,32 +146,6 @@ $comments = $db->db_select("", "article_id=" . "'" . $article_id . "'");
                 </div>
 
             </div><!-- /.blog-main -->
-
-
-
-
-            <div id="sidebar" class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                <div class="panel-heading">
-                    专题
-                </div>
-
-                <ul class="category-root list-group">
-                    <li class="list-group-item">
-                        <a href="/topic/1">旅游
-                        </a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="/topic/2">轻松
-                        </a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="/topic/5">测试专题
-                        </a>
-                    </li>
-                </ul>
-
-                </aside>
-            </div>
         </div>
     </div><!-- /.row -->
     </div><!-- /.container -->
